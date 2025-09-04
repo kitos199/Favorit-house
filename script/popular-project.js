634d93991e8c424f171478c2a1282bc4365d4ae7
Никита <nik-ta@mail.ru>
2025-09-05 01:16:42 +0300

    add refactoring js code

--- script/popular-project.js
+++ script/popular-project.js
@@ -3,21 +3,28 @@ const btnSwitchLeft = document.querySelector('.btn__switch--left');
 const popularProjectBtnSwitch = document.querySelectorAll(
   '.popular-project__btn__switch'
 );
-const wrapperProjectSketch=document.querySelector('.wrapper__project-sketch');
+const wrapperProjectSketch = document.querySelector('.wrapper__project-sketch');
 
-btnSwitchRight.addEventListener("click", ()=>{
-  wrapperProjectSketch.scrollBy({
-  left: 350,
-  top: 0,
-  behavior: 'smooth'
-});
-})
+// Перелистование картинок
+const scrolBtnFooter = function(e){
+   if (e.target.matches(".btn__switch--right")||e.target.matches("img.arrow-right")) {
+      wrapperProjectSketch.scrollBy({
+        left: 350,
+        top: 0,
+        behavior: 'smooth',
+      });
+    } else if (
+      e.target.matches('.btn__switch--left') ||
+      e.target.matches('img.arrow-left')
+    ) {
+      wrapperProjectSketch.scrollBy({
+        left: -350,
+        top: 0,
+        behavior: 'smooth',
+      });
+    }
+}
 
-
-btnSwitchLeft.addEventListener("click", ()=>{
-  wrapperProjectSketch.scrollBy({
-    left:-350,
-    top:0,
-    behavior:"smooth"
-  })
-})
+for (element of popularProjectBtnSwitch) {
+  element.addEventListener('click', scrolBtnFooter);
+}
